4f05bdc105f45b7a7e20052b6e07bfdb3e4672e0
OpenProgger <openproggerfreak@gmail.com>
2022-02-22 17:41:53 +0100

    Fix identation and README.md

--- README.md
+++ README.md
@@ -1,9 +1,22 @@
 # Minimal Lambda Calculus Compiler
 
-Supports only single char symbols, except 'l' which stands for a lambda function
+Supports only single char symbols, except 'l' which stands for a lambda function.
+'+' is used as unchirchifier function to make some simple calculations.
 
-Compile Compiler: clang -nostartfiles -nodefaultlibs -masm=intel -static x86.S -o LC
+Compile Compiler:  
+```
+cc -nostartfiles -nodefaultlibs -static compiler.c -o LC
+```
+If interested, bootstrap.S is a assembly-version of compiler.c
 
-Compile lambda calculus: cat input | ./LC > output.S
+Compile lambda calculus:  
+```
+cat input | ./LC > output.S
+```
 
-Create final binary: clang -nostartfiles -nodefaultlibs -masm=intel -static output.S runtime.S
+Create final binary:  
+```
+clang -nostartfiles -nodefaultlibs -masm=intel -static output.S runtime.S
+```
+
+The sample input file is copied from [Matt's Blog](https://matt.might.net/articles/compiling-up-to-lambda-calculus/) for testing.
--- compiler.c
+++ compiler.c
@@ -3,55 +3,76 @@
 
 int label_count;
 
-typedef struct List {
-	struct List * cdr;
-	struct List * car;
-} List;
+typedef struct Cell {
+    struct Cell* cdr;
+    struct Cell* car;
+} Cell;
 
+// Read single char
 static void* gettoken() {
-	char look = getchar();
-	while(look == ' ' || look == '\n') { look = getchar(); }
-	return (List*)(unsigned long)look;
+    char look = getchar();
+
+    //Ignore whitespace
+    while(look == ' ' || look == '\n') { look = getchar(); }
+
+    return (Cell*)(unsigned long)look;
 }
 
-void* getlist() {
-	List* token = gettoken();
-	if ((unsigned long)token == ')') return 0;
-	List* result = calloc(1, sizeof(List));
-	token = (unsigned long)token == '(' ? getlist() : token;
-	*result = (List){ .car = token, .cdr = getlist()};
-	return result;
+// Read cell
+static void* getcell() {
+    Cell* token = gettoken();
+
+    // If empty, return empty cell
+    if ((unsigned long)token == ')') return 0;
+
+    Cell* result = calloc(1, sizeof(Cell));
+
+    // Check for nested cell
+    token = (unsigned long)token == '(' ? getcell() : token;
+
+    *result = (Cell){ .car = token, .cdr = getcell()};
+    return result;
 }
 
-void compile(List *exp) {
-	if (((unsigned long)exp | 0xFF) == 0xFF ) {
-		printf("mov rdi, %lu\n", (unsigned long)exp);
-		printf("call get_val\n");
-	} else if ((unsigned long)exp->car == 'l') {
-		int label = label_count++;
-		printf("call L%d\n", label);
-		printf("mov rsi, rdi\n");
-		printf("mov rdi, %lu\n", (unsigned long)(exp->cdr->car->car));
-		printf("call set_var\n");
-		compile(exp->cdr->cdr->car);
-		printf("ret\n");
-		printf("L%d:\n", label);
-		printf("pop rdi\n");
-		printf("call gen_closure\n");
-	} else { /* function call */
-		compile(exp->car);
-		printf("push rax\n");
-		compile(exp->cdr->car);
-		printf("pop rdi\n");
-		printf("mov rsi, rax\n");
-		printf("call apply\n");
-	}
+// Compile lambda
+void compile(Cell *exp) {
+    // Check if symbol or cell
+    if (((unsigned long)exp | 0xFF) == 0xFF ) {
+        // Process symbol
+        printf("mov rdi, %lu\n", (unsigned long)exp);
+        printf("call get_val\n");
+    } else if ((unsigned long)exp->car == 'l') {
+        // Process lambda function
+        int label = label_count++;
+        printf("call L%d\n", label);
+        printf("mov rsi, rdi\n");
+        printf("mov rdi, %lu\n", (unsigned long)(exp->cdr->car->car));
+        printf("call set_var\n");
+        compile(exp->cdr->cdr->car);
+        printf("ret\n");
+        printf("L%d:\n", label);
+        printf("pop rdi\n");
+        printf("call gen_closure\n");
+    } else {
+        // Function call
+        compile(exp->car);
+        printf("push rax\n");
+        compile(exp->cdr->car);
+        printf("pop rdi\n");
+        printf("mov rsi, rax\n");
+        printf("call apply\n");
+    }
 }
 
 int main() {
-	printf(".globl app\napp:\n");
-	List* token = gettoken();
-	compile((unsigned long)token == '(' ? getlist() : token);
-	printf("ret\n");
-	return 0;
+    // Make GAS happy
+    printf(".globl app\napp:\n");
+
+    // Parse source
+    Cell* token = gettoken();
+
+    // Compile source
+    compile((unsigned long)token == '(' ? getcell() : token);
+    printf("ret\n");
+    return 0;
 }
